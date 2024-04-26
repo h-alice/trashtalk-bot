0adf6c471843d0e4989df3a14b8c8f46ee2dc40a
h-alice <admin@halice.art>
2024-04-26 14:08:21 +0800

    Funcion: Create stacked messages.

--- src/trainer/dataset_loader.py
+++ src/trainer/dataset_loader.py
@@ -17,6 +17,32 @@ def create_random_message_stack_from_adjacent_records(dataset: Dataset, stack_si
     message_idx = random.randint(min_message_idx, max_message_idx - stack_size + 1)
     return dataset['message'][message_idx:message_idx + stack_size]
 
+def create_message_stack(dataset: Dataset, batch_size: int, stack_size_min: int=2, stack_size_max: int=2, even_stack_size: bool=True) -> List[List[str]]:
+    """
+    ## Create Message Stack Batch
+    Create a message stack batch from the dataset.
+
+    The size of stack will be randomly selected between `stack_size_min` and `stack_size_max`.
+
+    Since the message is tend to be paired, the stack size is recommended to be even number.
+
+    Parameters:
+    - dataset: Dataset, The dataset.
+    - batch_size: int, The batch size.
+    - stack_size_min: int, The minimum size of the stack.
+    - stack_size_max: int, The maximum size of the stack.
+    - even_stack_size: bool, Whether the stack size should be even. Default is True.
+    """
+    
+    message_stacks = []
+    for _ in range(batch_size):
+        stack_size = random.randint(stack_size_min, stack_size_max)
+        if even_stack_size and stack_size % 2 != 0:
+            stack_size += 1
+        message_stack = create_random_message_stack_from_adjacent_records(dataset, stack_size)
+        message_stacks.append(message_stack)
+    return message_stacks
+
 if __name__ == "__main__": # For test.
     # Load dataset.
     dataset = load_dataset("h-alice/chat-cooking-master-boy-100k", split="train")
