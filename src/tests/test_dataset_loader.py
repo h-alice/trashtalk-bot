8a4f252d8840675ba8b2f8224f47853a104edd4b
h-alice <admin@halice.art>
2024-04-26 21:59:33 +0800

    Test case

--- src/tests/test_dataset_loader.py
+++ src/tests/test_dataset_loader.py
@@ -13,8 +13,20 @@ class TestDatasetLoader(unittest.TestCase):
         self.dataset = dataset
 
     def test_create_message_stack(self):
-        # NOTE: Actually this test doesn't have deterministic result. And should be checked by output.
+        # Load dataset.
+        self.test_loading_dataset_from_hf()
+        # NOTE: This test doesn't have deterministic result. And should be checked by output.
         print(create_message_stack(self.dataset, batch_size=10, stack_size_min=2, stack_size_max=2, even_stack_size=True))
+
+    def test_convert_message_stack_to_training_promots(self):
+        # Load dataset. 
+        self.test_loading_dataset_from_hf()
+
+        prompt_list = convert_message_stack_to_training_promots(
+            create_message_stack(self.dataset, batch_size=10, stack_size_min=2, stack_size_max=2, even_stack_size=True))
+        
+        for prompt in prompt_list:
+            print(prompt)
 
 
 if __name__ == '__main__':
