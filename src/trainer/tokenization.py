54d39789b7fafe2035126055c165248d584e3306
h-alice <admin@halice.art>
2024-04-27 23:37:42 +0800

    Add tokenizer.

--- src/trainer/tokenization.py
+++ src/trainer/tokenization.py
@@ -1,6 +1,7 @@
+from typing import List
 from transformers import AutoTokenizer, PreTrainedTokenizer
 
-def tokenize(tokenizer: PreTrainedTokenizer, text: str):
+def tokenize(tokenizer: PreTrainedTokenizer, text: str, eos_token: bool=False, max_length: int=2048) -> List[str]:
     """
     ## Tokenize Text
     Tokenize the text with the specified tokenizer.
@@ -13,8 +14,23 @@ def tokenize(tokenizer: PreTrainedTokenizer, text: str):
     - List[str], The list of tokens.
     """
 
-    return tokenizer.tokenize(text)
+    token_list = tokenizer(
+            text, # Prompt.
+            truncation=True, # Truncate the text if it exceeds the maximum length.
+            max_length=max_length, # Maximum length of the tokenized text.
+            padding=False, # No padding.
+        )
+    
+    if eos_token:
+        # Append the EOS token. 
+        # Note that it will append the token even if it exceeds the maximum length.
+        token_list["input_ids"].append(tokenizer.eos_token_id)
+        token_list["attention_mask"].append(1) # Set the attention mask to 1.
+
+    return token_list
+
+
 
 if __name__ == "__main__": # For test.
     tokenizer = AutoTokenizer.from_pretrained("/Users/h-alice/Desktop/gemma-2b")
-    print(tokenize(tokenizer, "來去菜市場看歐郎甲追夠"))
+    print(tokenize(tokenizer, "來去菜市場看歐郎甲追夠", eos_token=False))
